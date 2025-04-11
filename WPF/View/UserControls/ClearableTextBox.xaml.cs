74bd91bb62fb51631c482f13182120f747bc97d1
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-04-11 10:39:16 +0300

    Add some logic to codebehind for placeholder text

--- WPF/View/UserControls/ClearableTextBox.xaml.cs
+++ WPF/View/UserControls/ClearableTextBox.xaml.cs
@@ -11,10 +11,32 @@ namespace WPF.View.UserControls
             InitializeComponent();
         }
 
+        private string _placeHolder;
+
+        public string PlaceHolder
+        {
+            get => _placeHolder;
+            set { 
+                _placeHolder = value; 
+                // do not do this
+                tbPlaceHolder.Text = _placeHolder;
+                // OnPropertyChanged()
+            }
+        }
+
+
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
             txtInput.Clear();
             txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceHolder.Visibility = Visibility.Visible;
+             else 
+                tbPlaceHolder.Visibility = Visibility.Hidden;
         }
     }
 }
