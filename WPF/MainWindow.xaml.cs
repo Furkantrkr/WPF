ba5d2a2442fbef7edb83a39d2cb1ab2773b0b4f9
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-04-11 14:21:31 +0300

    Communicate with UI by using INotiftPropertyChanged interfaces.

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace WPF
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        bool IsRunning = false;
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
         }
 
-        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
-        {
-            if (!IsRunning)
-            {
-                tbStatus.Text = "Stopped";
-                btnToggleRun.Content = "Run";
-            } else
-            {
-                tbStatus.Text = "Running";
-                btnToggleRun.Content = "Stop";
-            }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-            IsRunning = !IsRunning;
+        private string _boundText;
+
+        public string BoundText
+        {
+            get { return _boundText; }
+            set
+            {
+                _boundText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
         }
     }
 }
