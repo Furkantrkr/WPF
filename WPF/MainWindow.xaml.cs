c56cbedbf21df92fa73b475da744a3c6ebb7ae81
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-04-11 09:40:03 +0300

    add userControl MenuBar in order to make ui modular and clean

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -4,25 +4,9 @@ namespace WPF
 {
     public partial class MainWindow : Window
     {
-        bool IsRunning = false;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
-        {
-            if (!IsRunning)
-            {
-                tbStatus.Text = "Stopped";
-                btnToggleRun.Content = "Run";
-            } else
-            {
-                tbStatus.Text = "Running";
-                btnToggleRun.Content = "Stop";
-            }
-
-            IsRunning = !IsRunning;
         }
     }
 }
