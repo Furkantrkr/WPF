a1154c2c3a0abf901211315699c5e59bf53a8eef
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-04-13 00:57:29 +0300

    Create new windows

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -4,25 +4,19 @@ namespace WPF
 {
     public partial class MainWindow : Window
     {
-        bool IsRunning = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
         {
-            if (!IsRunning)
-            {
-                tbStatus.Text = "Stopped";
-                btnToggleRun.Content = "Run";
-            } else
-            {
-                tbStatus.Text = "Running";
-                btnToggleRun.Content = "Stop";
-            }
 
-            IsRunning = !IsRunning;
+        }
+
+        private void btnModal_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ WPF/View/NormalWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WPF.View
+{
+    public partial class NormalWindow : Window
+    {
+        public NormalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
