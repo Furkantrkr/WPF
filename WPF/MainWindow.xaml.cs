8cab4e9f6da0e6be4bdbb6f1b3678cc81a25b737
Furkan TÜRKER <furkantrkr13@gmail.com>
2025-04-12 22:15:07 +0300

    Init

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -4,25 +4,14 @@ namespace WPF
 {
     public partial class MainWindow : Window
     {
-        bool IsRunning = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (!IsRunning)
-            {
-                tbStatus.Text = "Stopped";
-                btnToggleRun.Content = "Run";
-            } else
-            {
-                tbStatus.Text = "Running";
-                btnToggleRun.Content = "Stop";
-            }
-
-            IsRunning = !IsRunning;
+            DragMove();
         }
     }
 }
