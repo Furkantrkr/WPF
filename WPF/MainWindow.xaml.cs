21a430efc7fc5b14ad0a8ae7c3acf9df55fdbb24
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-04-13 13:54:13 +0300

    Set owner window to model one

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using WPF.View;
 
 namespace WPF
 {
@@ -11,12 +12,18 @@ namespace WPF
 
         private void btnNormal_Click(object sender, RoutedEventArgs e)
         {
-
+            NormalWindow normalWindow = new NormalWindow();
+            normalWindow.Show();
         }
 
         private void btnModal_Click(object sender, RoutedEventArgs e)
         {
-
+            ModalWindow modalWindow = new ModalWindow(this);
+            Opacity = 0.4;
+            modalWindow.ShowDialog();
+            Opacity = 1;
+            if (modalWindow.Success)
+                txtInput.Text = modalWindow.Input;                 
         }
     }
 }
--- WPF/View/ModalWindow.xaml.cs
+++ WPF/View/ModalWindow.xaml.cs
@@ -1,12 +1,35 @@
-﻿using System.Windows;
+﻿using System.Security.Policy;
+using System.Windows;
 
 namespace WPF.View
 {
     public partial class ModalWindow : Window
     {
-        public ModalWindow()
+        public bool Success { get; set; }
+        public string Input { get; set; }
+        public ModalWindow(Window parentWindow)
         {
+            Owner = parentWindow;
             InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            Success = true;
+            Input = txtInput.Text;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void txtInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtInput.Text))
+                btnOK.IsEnabled = true;
+            else btnOK.IsEnabled = false;
         }
     }
 }
