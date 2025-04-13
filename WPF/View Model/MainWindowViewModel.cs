39da0b53ec14782ac4263b63fe3de0c7e00bb79c
Furkan Turker <furkan.turker@flatlands.com.tr>
2025-04-13 17:33:12 +0300

    set a mvvm pattern between model and ui

--- WPF/View Model/MainWindowViewModel.cs
+++ WPF/View Model/MainWindowViewModel.cs
@@ -1,7 +1,37 @@
-﻿namespace WPF.View_Model
-{
-    public class MainWindowViewModel
-    {
+﻿using System.Collections.ObjectModel;
+using WPF.Model;
+using WPF.MVVM;
 
+namespace WPF.View_Model
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items {  get; set; }  
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+
+            Items.Add(new Item()
+            {
+                Name = "Product1",
+                SerialNumber = "0001",
+                Quantity = 5
+            });
+            Items.Add(new Item()
+            {
+                Name = "Product2",
+                SerialNumber = "0002",
+                Quantity = 6
+            });
+        }
+        private Item _selectedItem;
+        public Item SelectedItem
+        {
+            get { return _selectedItem; }
+            set { 
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
